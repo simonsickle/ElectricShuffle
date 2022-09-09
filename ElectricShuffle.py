aba5428b302677c656bb32cbb336f7b2db84efe5
Simon Sickle <51972200+simonsickle@users.noreply.github.com>
2022-09-08 22:31:57 -0400

    Init

---
+++ ElectricShuffle.py
@@ -0,0 +1,80 @@
+#!/bin/env python3
+
+import datetime
+import json
+import os
+from os import path
+import random
+from select import select
+import tidalapi
+
+dir = path.dirname(path.realpath(__file__))
+credential_file = path.join(dir, '.credentials.json')
+
+need_oauth = True
+session = tidalapi.Session()
+
+# Check if the credentials are still valid
+if os.path.exists(credential_file):
+    with open(credential_file, "r") as open_file:
+        token_json = json.load(open_file)
+
+    # Check tokens with tidal
+    session.load_oauth_session(
+        token_json["token_type"],
+        token_json["access_token"],
+        token_json["refresh_token"],
+        datetime.datetime.fromisoformat(token_json["expiry_time"])
+    )
+    
+if not session.check_login():
+    # Have user go log in and when this completes, we will auto continue
+    session.login_oauth_simple()
+
+    session_dict = {
+        "token_type": session.token_type,
+        "access_token": session.access_token,
+        "refresh_token": session.refresh_token,
+        "expiry_time": session.expiry_time.isoformat()
+    }
+    with open(credential_file, "w") as out_file:
+        out_file.write(json.dumps(session_dict))
+
+### Now that session is handled, get playlists
+playlists = session.user.playlists()
+print("Please select the playlist you wish to shuffle by intering it's number below")
+for idx, playlist in enumerate(playlists):
+    print(f"  [{idx+1}] {playlist.name}")
+
+# Get playlists on user account
+playlist_num = int(input("Playlist Number: ")) - 1
+
+selected_playlist = playlists[playlist_num]
+print(f"Electric shuffle {selected_playlist.name}?")
+should_continue = bool(input("Continue (Yes/No): "))
+
+### Allow bailing
+if not should_continue:
+    exit(1)
+
+
+### Do the thing
+tracklist = selected_playlist.tracks()
+track_ids = []
+
+for track in tracklist:
+    track_ids.append(track.id)
+
+### Signify that this is a copy for the user
+shuffled_name = f"{selected_playlist.name} - Electrified"
+for playlist in playlists:
+    ### Delete an existing version of this playlist if we are re-rolling
+    if playlist.name is shuffled_name:
+        playlist.delete()
+
+## Create (or re create) the electric playlist
+new_playlist = session.user.create_playlist(shuffled_name, selected_playlist.description)
+
+## Add the beautiful music after a quick shuffle
+random.shuffle(track_ids)
+new_playlist.add(track_ids)
---
+++ README.md
@@ -0,0 +1,27 @@
+### Electric Shuffle
+
+This is a (terrible) app to shuffle your Tidal playlists since there are not (currently) any native ways to due so on a Tesla. Hopefully it will be depreciated in the near future and we can go back to just shuffling in the car :)
+
+## WARNING, DRAGONS
+
+I'm not responsible for bricked devices, dead SD cards, thermonuclear war, or you getting fired because your entire playlist got deleted and you are now too sad to work. This _shouldn't happen_ but the app is very lightly tested.
+
+### Running The Program
+
+1. Clone this repo to your computer
+1. Create a venv to install packages by running `python3 -m venv .venv`
+1. Start using the virtual env by running `source .venv/bin/activate`
+1. Install required packages by running `pip3 install -r requirements.txt`
+1. Run the program `./ElectricShuffle.py`
+1. Log in by clicking the link (or copying it to your browser). Once logged in return to the terminal.
+1. Pick a playlist to shuffle
+1. Confirm that is the playlist you want
+1. Go enjoy some tunes! Notice your new playlist which is a copy of your current one but with an " - Electrified" suffix. This is the shuffled one.
+
+### Contributing
+
+Contributions are welcome, this is just a little hobby script I wrote to make my life easier. Feel free to change it and send a pull request.
+
+### Disclaimer
+
+While I work at Block as of writing this, I do not work on the Tidal team. This is ENTIRELY unofficial and unsupported and is my own work.
